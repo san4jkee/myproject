65761021ecff4019431006b52e89e36c1e132f89
san4jkee <san4ezudm@yandex.ru>
2020-03-22 19:55:30 +0300

    day03

---
+++ day03/masked.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int	main()
+{
+	char masked_raider[] = "Живой!";
+	char *jimmy = masked_raider;
+	printf("Грабитель в маске сейчас %s Джимми сейчас%s\n", masked_raider, jimmy);
+	masked_raider[0] = 'М';
+	masked_raider[1] = 'Е';
+	masked_raider[2] = 'Р';
+	masked_raider[3] = 'Т';
+	masked_raider[4] = 'В';
+	masked_raider[5] = '!';
+	printf("Грабитель в маске сейчас %s Джимми сейчас %s\n", masked_raider, jimmy);
+	return 0;
+}
